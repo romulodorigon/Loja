090b83df068b00a4a4e5080c3a42f79d6fb4504f
Romulo Quinteiro <romuloquinteiro@gmail.com>
2017-05-01 17:21:41 -0300

    Adicionado classe Autores para relacionamento com livros

--- src/main/java/br/com/casadocodigo/loja/beans/AdminLivrosBean.java
+++ src/main/java/br/com/casadocodigo/loja/beans/AdminLivrosBean.java
@@ -1,11 +1,15 @@
 package br.com.casadocodigo.loja.beans;
 
+import java.util.Arrays;
+import java.util.List;
+
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.transaction.Transactional;
 
 import br.com.casadocodigo.loja.dao.LivroDao;
+import br.com.casadocodigo.loja.models.Autor;
 import br.com.casadocodigo.loja.models.Livro;
 
 // CDI
@@ -29,6 +33,10 @@ public class AdminLivrosBean {
 	public void salvar() {
 		dao.salvar(livro);
 		System.out.println("Livro cadastrado: " + livro);
+	}
+
+	public List<Autor> getAutores() {
+		return Arrays.asList(new Autor(1, "Paulo Silveira"), new Autor(2, "Sergio Lopes"));
 	}
 
 	public Livro getLivro() {
